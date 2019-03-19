8adfc031ebc3c3f44187feb45519d0c86306ee63
Slawomir <zawistowski.slawomir@gmail.com>
2019-03-19 23:08:46 +0100

    Add styled-components and normalize.css
    Normalize added as a global style of styled components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
 
+import { NormalizeStyles } from './../styles/normalize'
+
 const App = () => {
   return (
     <div className="App">
+      <NormalizeStyles />
       <h1>Sample heading</h1>
       <p>Sample text.</p>
     </div>
