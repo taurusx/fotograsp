efcd7763c082f82d4ae723840e2b90730de52717
Slawomir <zawistowski.slawomir@gmail.com>
2019-03-22 16:26:18 +0100

    Modify Layout to request latest collections
    Requested are: latest collections and their first photos

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { Switch, Route } from 'react-router-dom'
 
@@ -6,6 +6,13 @@ import Header from './Header'
 import Main from './Main'
 import Collections from './Collections'
 import Gallery from './Gallery'
+import { unsplash } from './../utils/apiSimulation'
+import {
+  getCollections,
+  getPhotos,
+  parseCollectionsDetails,
+  parsePhotosDetails,
+} from './../utils/helpersApi'
 
 const LayoutWrapper = styled.div`
   margin: 0 auto;
@@ -15,15 +22,79 @@ const LayoutWrapper = styled.div`
 `
 
 const Layout = ({ children }) => {
+  const [collectionsArray, setCollectionsArray] = useState([])
+  const [jsonReady, setJsonReady] = useState(false)
+
+  useEffect(() => {
+    if (!jsonReady) {
+      setJsonReady(true)
+      let a = getCollections(unsplash)
+        .then(result => {
+          let array = parseCollectionsDetails(result)
+          setCollectionsArray(array)
+          return array
+        })
+        .catch(err => new Error(err))
+    }
+  }, [jsonReady])
+
+  const [collectionsWithPhotos, setCollectionsWithPhotos] = useState([])
+  const [photosReady, setPhotosReady] = useState(false)
+
+  useEffect(() => {
+    if (collectionsArray.length > 0) {
+      let collections = []
+      let flag = 0
+      collectionsArray.forEach((collection, index) => {
+        const { id } = collection
+        let currentCollection = { ...collection }
+        const images = getPhotos(unsplash, id)
+          .then(photos => {
+            return parsePhotosDetails(photos)
+          })
+          .then(photos => {
+            currentCollection.images = photos
+            return currentCollection
+          })
+          .then(collection => {
+            collections[index] = collection
+            flag++
+            setCollectionsWithPhotos(collections)
+          })
+          .then(() => {
+            if (collectionsArray.length === flag) setPhotosReady(true)
+          })
+      })
+    }
+  }, [collectionsArray.length])
+
   return (
     <LayoutWrapper>
       <Header />
       {children}
       <Main>
-        <Switch>
-          <Route exact path="/" component={Collections} />
-          <Route path="/collections/:id" component={Gallery} />
-        </Switch>
+        {photosReady ? (
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <Collections
+                  {...props}
+                  collectionsArray={collectionsWithPhotos}
+                />
+              )}
+            />
+            <Route
+              path="/collections/:id"
+              render={props => (
+                <Gallery {...props} collectionsArray={collectionsWithPhotos} />
+              )}
+            />
+          </Switch>
+        ) : (
+          <div>Łączymy się z bazą...</div>
+        )}
       </Main>
     </LayoutWrapper>
   )
