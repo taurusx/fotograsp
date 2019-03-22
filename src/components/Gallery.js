c01000797743777e4484994b06c1642b4cd20120
Slawomir <zawistowski.slawomir@gmail.com>
2019-03-22 16:46:39 +0100

    Add Gallery views
    Modified Gallery to display grid of GalleryCards
    with photos for current Gallery.

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,18 +2,51 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 
+import GalleryCard from './GalleryCard'
+
 const GalleryWrapper = styled.section`
-  max-width: 100%;
+  width: 100%;
   color: ${props => props.theme.fgTopView};
   background-color: ${props => props.theme.bgTopView};
 `
 
-const Gallery = () => {
+const GalleryGrid = styled.article`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 400px));
+  grid-auto-rows: 400px;
+  justify-content: space-around;
+
+  @media (max-width: 480px) {
+    grid-gap: 10px;
+  }
+`
+
+const StyledLink = styled(Link)`
+  color: ${props => props.theme.foreground};
+
+  &:hover {
+    color: ${props => props.theme.accent};
+  }
+`
+
+const Gallery = ({ match, collectionsArray }) => {
+  const id = match.params.id
+  console.log(id)
+  const currentCollection = collectionsArray.filter(collection => {
+    return collection.id == id
+  })[0]
+  console.log(currentCollection)
+  const { images, title, total_photos } = currentCollection
   return (
     <GalleryWrapper>
-      <h1>Tytuł galerii</h1>
-      <p>(Lista zdjęć)</p>
-      <Link to="/">Powrót</Link>
+      <h1>{title}</h1>
+      <GalleryGrid>
+        {images.map(photo => (
+          <GalleryCard key={photo.id} photo={photo} title={title} />
+        ))}
+      </GalleryGrid>
+      <StyledLink to="/">Powrót</StyledLink>
     </GalleryWrapper>
   )
 }
---
+++ src/components/GalleryCard.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ImageCard = styled.div`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  padding: 0.5rem;
+  background-color: ${props => props.theme.bgTopView};
+  box-shadow: 5px 5px 10px ${props => props.theme.fgTopView};
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    background-color: ${props => props.color};
+  }
+
+  &:hover {
+    cursor: zoom-in;
+    background-color: ${props => props.color};
+    box-shadow: 5px 5px 20px;
+  }
+`
+
+const GalleryCard = ({ photo, title }) => {
+  const { id, color, urls, likes, description } = photo
+  const { small, regular } = urls
+  return (
+    <ImageCard regular={regular} id={id} color={color} likes={likes}>
+      <img src={small} alt={description || title} />
+    </ImageCard>
+  )
+}
+
+export default GalleryCard
