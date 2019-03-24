1ff14b4e72bdd9fe8bbb20c90a2267665f4f7f04
Slawomir <zawistowski.slawomir@gmail.com>
2019-03-24 13:12:21 +0100

    Add Netlify redirects for using history pushstate
    Added copy-webpack-plugin.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const DotenvPlugin = require('dotenv-webpack')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
@@ -8,5 +9,13 @@ module.exports = merge(common, {
     new DotenvPlugin({
       path: `./.env.production`,
     }),
+    new CopyPlugin([
+      {
+        from: './public/_redirects',
+        to: './_redirects',
+        toType: 'file',
+        flatten: true,
+      },
+    ]),
   ],
 })
